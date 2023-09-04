e78f0aed7d52b56fe1ae9f59a9d8c477bcea96e6
Pavel <astr.efremov@gmail.com>
2023-09-04 21:16:10 +0400

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ firstMain/src/main/java/com/firstProject/firstMain/FirstMainApplication.java
@@ -0,0 +1,13 @@
+package com.firstProject.firstMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMainApplication.class, args);
+	}
+
+}
---
+++ firstMain/src/test/java/com/firstProject/firstMain/FirstMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstProject.firstMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
